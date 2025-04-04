2756472f6b3989f07ddb8f12ef76dbf5b358bf91
madhugitlearn <madhumathi.nkv@gmail.com>
2025-04-04 07:00:20 +0530

    feat:removed backend block

--- main.tf
+++ main.tf
@@ -13,14 +13,14 @@ provider "aws" {
   #profile = "developer" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
   region  = "us-east-1"
 }
-#Backend Block
-terraform {
-  backend "s3" {
-    bucket         = "madhutfstate"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-  }
-}
+# #Backend Block
+# terraform {
+#   backend "s3" {
+#     bucket         = "madhutfstate"
+#     key            = "terraform.tfstate"
+#     region         = "us-east-1"
+#   }
+# }
 
 # Resource Block
 resource "aws_instance" "ec2demo" {
