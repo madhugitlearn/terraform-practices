f576939330b5d62a288bc30e48e69ccc56e54637
madhugitlearn <madhumathi.nkv@gmail.com>
2025-04-07 21:26:39 +0530

    changed bucket name

--- terraform-ec2/backend.tf
+++ terraform-ec2/backend.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-tf-state-files-bucket"         # <-- Replace
+    bucket         = "madhutfstate"       
+  # <-- Replace
     key            = "terraform/ec2/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
